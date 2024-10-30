60c68639e91afcb013982de4ba5b8af3ca1961b7
JVgamer717 <717652@student.dusd.net>
2024-10-30 11:46:39 -0700

    2dgamekitbox coliders
    I worked and finished on my 2d box coliders but I

--- 2DBeginnerAdventureVasquezp4/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureVasquezp4/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>(); 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + 8.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 8.0f * vertical * Time.deltaTime;
-        
-        transform.position = position;
 
     }
+    private void FixedUpdate()
+    {
+
+
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 8.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 8.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
 }
