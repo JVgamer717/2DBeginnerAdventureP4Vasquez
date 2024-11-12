e9184bb9d0a627248703c77f1e1950279cb11dad
JVgamer717 <717652@student.dusd.net>
2024-11-12 11:46:00 -0800

    WORKED ON PROJECT
    WORKED ON PROJECT

--- 2DBeginnerAdventureVasquezp4/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureVasquezp4/Assets/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -10,6 +11,8 @@ public class EnemyController : MonoBehaviour
     public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2d;
+
+    bool broken = true;
 
     float timer;
     int direction = 1;
@@ -27,6 +30,11 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
         if (timer < 0)
         {
@@ -39,7 +47,10 @@ public class EnemyController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-
+        if(!broken)
+        {
+            return;
+        }
 
         Vector2 position = rigidbody2d.position;
         if (vertical)
@@ -71,4 +82,13 @@ public class EnemyController : MonoBehaviour
 
 
     }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2d.simulated = false;
+        animator.SetTrigger("Fixed");
+
+    }
+
 }
--- 2DBeginnerAdventureVasquezp4/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureVasquezp4/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 8.0f;
     public float timeInvincible = 2.0f;
     public int maxHealth = 5;
+
+    public GameObject projectilePrefab;
+
+
     public int health { get { return currentHealth; } }
     public int currentHealth;
 
@@ -57,6 +61,12 @@ public class PlayerController : MonoBehaviour
                 isInvincible = false;
             }
         }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
     }
     private void FixedUpdate()
     {
@@ -87,4 +97,17 @@ public class PlayerController : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
+
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+    }
+      
+
 }
